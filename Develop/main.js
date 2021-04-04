d8e2043af33004c6475a0af86cd773f6ca507ddb
Vilma Qerama <vilma.qerama@hotmail.com>
2021-04-04 23:03:09 +0100

    add the if conditions to evaluate the timeblockTimes agains the current time

--- Develop/main.js
+++ Develop/main.js
@@ -1,19 +1,48 @@
-const getCurrentDateTime = () => {
-  const dateTime = moment().format("Do [of] MMMM, YYYY, HH:mm:ss");
-  return dateTime;
+//render current date in the required format: Day, Date
+const displayCurrentDate = () => {
+  const dateTime = $("#currentDay");
+  const displayNow = moment().format("dddd, Do [of] MMMM YYYY");
+  dateTime.text(displayNow);
 };
 
-const startTimer = () => {
-  const timerTick = () => {
-    const dateTimeText = getCurrentDateTime();
-    $(".clock").text(dateTimeText);
-  };
+const renderCalendarEvents = () => {
+  //get data from local
+  const plannerEvents = localStorage.getItem("plannerEvents");
 
-  setInterval(timerTick, 1000);
+  if (plannerEvents !== null) {
+    // const currentHour = moment().hour();
+    const currentHour = 11;
+    const timeBlocks = $(".container .row");
+    const callback = function () {
+      const timeBlockTime = Number.parseInt($(this).data("time"), 10);
+      if (timeBlockTime === currentHour) {
+        // use the find method to find the textarea and replace class past with present
+        $(this).find("textarea").removeClass("past").addClass("present");
+      }
+      if (timeBlockTime > currentHour) {
+        // use the find method to find the textarea and replace class past with future
+        $(this).find("textarea").removeClass("past").addClass("future");
+      }
+    };
+    console.log(timeBlocks);
+    timeBlocks.each(callback);
+    // console.log();
+  } else {
+    localStorage.setItem("plannerEvents", JSON.stringify({}));
+  }
+  //get from the local storage
+
+  //check if data is null
+
+  //render the  data in the planner
+
+  //
+};
+const onReady = () => {
+  console.log("I am ready");
+  displayCurrentDate();
+
+  renderCalendarEvents();
 };
 
-const main = () => {
-  startTimer();
-};
-
-$("document").ready(main);
+$("document").ready(onReady);
